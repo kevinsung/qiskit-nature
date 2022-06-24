e2144315fee56d6c5cd4fdf88e449c1a75de73ed
Kevin J. Sung <kevjsung@umich.edu>
2022-06-24 10:45:33 -0400

    add trotter step

---
+++ qiskit_nature/circuit/library/low_rank_trotter.py
@@ -0,0 +1,203 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Trotter Hamiltonian simulation via low rank decomposition."""
+
+from __future__ import annotations
+
+import itertools
+from collections.abc import Iterator
+from typing import Optional, Sequence
+
+import numpy as np
+from qiskit import QuantumCircuit, QuantumRegister
+from qiskit.circuit import Instruction, Qubit
+from qiskit.circuit.library import RZGate, RZZGate
+
+from qiskit_nature.circuit.library import BogoliubovTransform
+from qiskit_nature.converters.second_quantization import QubitConverter
+from qiskit_nature.mappers.second_quantization import JordanWignerMapper
+from qiskit_nature.operators.second_quantization import QuadraticHamiltonian
+from qiskit_nature.utils import low_rank_decomposition
+
+
+class SimulateTrotterLowRank(QuantumCircuit):
+    r"""Trotter Hamiltonian simulation via low rank decomposition.
+
+    References:
+        - `arXiv:1808.02625`_
+        - `arXiv:2104.08957`_
+
+    .. _arXiv:1808.02625: https://arxiv.org/abs/1808.02625
+    .. _arXiv:2104.08957: https://arxiv.org/abs/2104.08957
+    """
+
+    def __init__(
+        self,
+        one_body_tensor: np.ndarray,
+        two_body_tensor: np.ndarray,
+        time: float,
+        final_rank: Optional[int] = None,
+        *,
+        n_steps: int = 1,
+        qubit_converter: Optional[QubitConverter] = None,
+        **circuit_kwargs,
+    ) -> None:
+        r"""
+        Args:
+            one_body_tensor: The one-body tensor of the Hamiltonian.
+            two_body_tensor: The two-body tensor of the Hamiltonian.
+            time: Simulation time.
+            qubit_converter: The qubit converter. The default behavior is to create
+                one using the call `QubitConverter(JordanWignerMapper())`.
+            circuit_kwargs: Keyword arguments to pass to the QuantumCircuit initializer.
+        """
+        if qubit_converter is None:
+            qubit_converter = QubitConverter(JordanWignerMapper())
+
+        n, _ = one_body_tensor.shape
+        register = QuantumRegister(n)
+        super().__init__(register, **circuit_kwargs)
+
+        if isinstance(qubit_converter.mapper, JordanWignerMapper):
+            operations = _simulate_trotter_low_rank(
+                register, one_body_tensor, two_body_tensor, time, final_rank, n_steps
+            )
+            for gate, qubits in operations:
+                self.append(gate, qubits)
+        else:
+            raise NotImplementedError(
+                "Currently, only the Jordan-Wigner Transform is supported. "
+                "Please use "
+                "qiskit_nature.mappers.second_quantization.JordanWignerMapper "
+                "to construct the qubit mapper."
+            )
+
+
+def _simulate_trotter_low_rank(
+    register: QuantumRegister,
+    one_body_tensor: np.ndarray,
+    two_body_tensor: np.ndarray,
+    time: float,
+    final_rank: Optional[int],
+    n_steps: int,
+) -> Iterator[tuple[Instruction, Sequence[Qubit]]]:
+    step_time = time / n_steps
+    for _ in range(n_steps):
+        yield AsymmetricLowRankTrotterStep(
+            one_body_tensor, two_body_tensor, step_time, final_rank=final_rank
+        ), register
+
+
+class AsymmetricLowRankTrotterStep(QuantumCircuit):
+    r"""Trotter Hamiltonian simulation via low rank decomposition.
+
+    References:
+        - `arXiv:1808.02625`_
+        - `arXiv:2104.08957`_
+
+    .. _arXiv:1808.02625: https://arxiv.org/abs/1808.02625
+    .. _arXiv:2104.08957: https://arxiv.org/abs/2104.08957
+    """
+
+    def __init__(
+        self,
+        one_body_tensor: np.ndarray,
+        two_body_tensor: np.ndarray,
+        time: float,
+        final_rank: Optional[int],
+        *,
+        qubit_converter: Optional[QubitConverter] = None,
+        **circuit_kwargs,
+    ) -> None:
+        r"""
+        Args:
+            one_body_tensor: The one-body tensor of the Hamiltonian.
+            two_body_tensor: The two-body tensor of the Hamiltonian.
+            time: Simulation time.
+            qubit_converter: The qubit converter. The default behavior is to create
+                one using the call `QubitConverter(JordanWignerMapper())`.
+            circuit_kwargs: Keyword arguments to pass to the QuantumCircuit initializer.
+        """
+        if qubit_converter is None:
+            qubit_converter = QubitConverter(JordanWignerMapper())
+
+        n, _ = one_body_tensor.shape
+        register = QuantumRegister(n)
+        super().__init__(register, **circuit_kwargs)
+
+        if isinstance(qubit_converter.mapper, JordanWignerMapper):
+            operations = _asymmetric_low_rank_trotter_step_jw(
+                register, one_body_tensor, two_body_tensor, time, final_rank
+            )
+            for gate, qubits in operations:
+                self.append(gate, qubits)
+        else:
+            raise NotImplementedError(
+                "Currently, only the Jordan-Wigner Transform is supported. "
+                "Please use "
+                "qiskit_nature.mappers.second_quantization.JordanWignerMapper "
+                "to construct the qubit mapper."
+            )
+
+
+def _asymmetric_low_rank_trotter_step_jw(
+    register: QuantumRegister,
+    one_body_tensor: np.ndarray,
+    two_body_tensor: np.ndarray,
+    time: float,
+    final_rank: Optional[int],
+) -> Iterator[tuple[Instruction, Sequence[Qubit]]]:
+    n_qubits = len(register)
+    corrected_one_body_tensor, leaf_tensors, core_tensors = low_rank_decomposition(
+        one_body_tensor, two_body_tensor, final_rank=final_rank
+    )
+
+    # compute basis change that diagonalizes one-body term
+    transformation_matrix, orbital_energies, _ = QuadraticHamiltonian(
+        corrected_one_body_tensor
+    ).diagonalizing_bogoliubov_transform()
+
+    # change to the basis in which the one-body term is diagonal
+    yield BogoliubovTransform(transformation_matrix.T.conj()), register
+
+    # simulate the one-body terms
+    for qubit, energy in zip(register, orbital_energies):
+        yield RZGate(-energy * time), (qubit,)
+
+    # simulate the two-body terms
+    prior_transformation_matrix = transformation_matrix
+    for leaf_tensor, core_tensor in zip(leaf_tensors, core_tensors):
+        # change basis
+        merged_transformation_matrix = prior_transformation_matrix @ leaf_tensor.conj()
+        yield BogoliubovTransform(merged_transformation_matrix), register
+        # simulate off-diagonal two-body terms
+        for p, q in itertools.combinations(range(n_qubits), 2):
+            yield from _rot11((register[p], register[q]), -2 * core_tensor[p, q] * time)
+        # simulate diagonal two-body terms
+        for p in range(n_qubits):
+            yield RZGate(-core_tensor[p, p] * time), (register[p],)
+        # update prior basis change matrix
+        prior_transformation_matrix = leaf_tensor.T
+
+    # undo final basis change
+    yield BogoliubovTransform(prior_transformation_matrix), register
+
+
+def _rot11(qubits: Sequence[Qubit], angle: float) -> Iterator[tuple[Instruction, Sequence[Qubit]]]:
+    """Phases the |11⟩ state by angle, up to global phase."""
+    # NOTE global phase is omitted
+    # TODO implement this as a gate with global phase added
+    a, b = qubits
+    yield RZGate(0.5 * angle), (a,)
+    yield RZGate(0.5 * angle), (b,)
+    yield RZZGate(-0.5 * angle), (a, b)
---
+++ test/circuit/library/test_low_rank_trotter.py
@@ -0,0 +1,109 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Test low rank Trotter step."""
+
+from test import QiskitNatureTestCase
+
+import numpy as np
+import scipy.sparse.linalg
+from ddt import data, ddt, unpack
+from qiskit import QuantumCircuit, QuantumRegister
+from qiskit.quantum_info import (
+    Operator,
+    Statevector,
+    random_hermitian,
+    random_statevector,
+    random_unitary,
+    state_fidelity,
+)
+
+from qiskit_nature.circuit.library import BogoliubovTransform
+from qiskit_nature.circuit.library.low_rank_trotter import SimulateTrotterLowRank
+from qiskit_nature.converters.second_quantization import QubitConverter
+from qiskit_nature.hdf5 import load_from_hdf5
+from qiskit_nature.mappers.second_quantization import BravyiKitaevMapper, JordanWignerMapper
+from qiskit_nature.operators.second_quantization.quadratic_hamiltonian import QuadraticHamiltonian
+from qiskit_nature.properties.second_quantization.electronic import ElectronicEnergy
+from qiskit_nature.properties.second_quantization.electronic.bases import ElectronicBasis
+from qiskit_nature.properties.second_quantization.electronic.integrals import (
+    OneBodyElectronicIntegrals,
+    TwoBodyElectronicIntegrals,
+)
+from test.random import random_two_body_tensor
+
+
+class TestAsymmetricLowRankTrotterStep(QiskitNatureTestCase):
+    """Tests for asymmetric low rank Trotter step."""
+
+    def test_asymmetric_low_rank_trotter_step_random(self):
+        """Test asymmetric low rank Trotter step."""
+        time = 0.1
+
+        # generate Hamiltonian
+        n_orbitals = 4
+        one_body_tensor = np.array(random_hermitian(n_orbitals))
+        two_body_tensor = random_two_body_tensor(n_orbitals, real=True, chemist=True)
+
+        one_body_integrals = OneBodyElectronicIntegrals(ElectronicBasis.SO, one_body_tensor)
+        two_body_integrals = TwoBodyElectronicIntegrals(ElectronicBasis.SO, 0.5 * two_body_tensor)
+        electronic_energy = ElectronicEnergy([one_body_integrals, two_body_integrals])
+        hamiltonian = electronic_energy.second_q_ops()["ElectronicEnergy"]
+
+        # generate random initial state
+        n_modes = hamiltonian.register_length
+        initial_state = random_statevector(2**n_modes)
+
+        # simulate exact evolution
+        hamiltonian_sparse = JordanWignerMapper().map(hamiltonian).to_spmatrix()
+        exact_state = scipy.sparse.linalg.expm_multiply(
+            -1j * time * hamiltonian_sparse, np.array(initial_state)
+        )
+
+        # make sure time is not too small
+        assert state_fidelity(exact_state, initial_state) < 0.95
+
+        # simulate Trotter evolution
+        circuit = SimulateTrotterLowRank(one_body_tensor, two_body_tensor, time, n_steps=500)
+        final_state = initial_state.evolve(circuit)
+        fidelity = state_fidelity(final_state, exact_state)
+        self.assertGreater(fidelity, 0.95)
+
+    # def test_asymmetric_low_rank_trotter_step_h2(self):
+    #     """Test asymmetric low rank Trotter step."""
+    #     time = 1.0
+
+    #     # load Hamiltonian
+    #     result = load_from_hdf5("test/transformers/second_quantization/electronic/H2_sto3g.hdf5")
+    #     energy = result.get_property("ElectronicEnergy")
+    #     one_body_tensor = energy.get_electronic_integral(ElectronicBasis.MO, 1).get_matrix()
+    #     two_body_tensor = 2 * energy.get_electronic_integral(ElectronicBasis.MO, 2).get_matrix()
+    #     hamiltonian = energy.second_q_ops()["ElectronicEnergy"]
+
+    #     # generate random initial state
+    #     n_modes = hamiltonian.register_length
+    #     initial_state = random_statevector(2**n_modes)
+
+    #     # simulate exact evolution
+    #     hamiltonian_sparse = JordanWignerMapper().map(hamiltonian).to_spmatrix()
+    #     exact_state = scipy.sparse.linalg.expm_multiply(
+    #         -1j * time * hamiltonian_sparse, np.array(initial_state)
+    #     )
+
+    #     # make sure time is not too small
+    #     assert state_fidelity(exact_state, initial_state) < 0.95
+
+    #     # simulate Trotter evolution
+    #     circuit = SimulateTrotterLowRank(one_body_tensor, two_body_tensor, time, n_steps=10)
+    #     final_state = initial_state.evolve(circuit)
+    #     fidelity = state_fidelity(final_state, exact_state)
+    #     self.assertGreater(fidelity, 0.95)
