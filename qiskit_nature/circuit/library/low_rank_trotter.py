e76cb10f92cb01c08367419f2ba99f8bd61f8021
Kevin J. Sung <kevjsung@umich.edu>
2022-06-27 11:36:12 -0400

    precompute low rank decomposition in trotter step

--- qiskit_nature/circuit/library/low_rank_trotter.py
+++ qiskit_nature/circuit/library/low_rank_trotter.py
@@ -70,9 +70,10 @@ class SimulateTrotterLowRank(QuantumCircuit):
         super().__init__(register, **circuit_kwargs)
 
         if isinstance(qubit_converter.mapper, JordanWignerMapper):
-            operations = _simulate_trotter_low_rank(
-                register, one_body_tensor, two_body_tensor, time, n_steps, final_rank, spin_basis
+            trotter_step = AsymmetricLowRankTrotterStepJW(
+                one_body_tensor, two_body_tensor, final_rank=final_rank, spin_basis=spin_basis
             )
+            operations = _simulate_trotter(register, trotter_step, time, n_steps)
             for gate, qubits in operations:
                 self.append(gate, qubits)
         else:
@@ -84,27 +85,19 @@ class SimulateTrotterLowRank(QuantumCircuit):
             )
 
 
-def _simulate_trotter_low_rank(
+# TODO add higher-order Trotter formulas
+def _simulate_trotter(
     register: QuantumRegister,
-    one_body_tensor: np.ndarray,
-    two_body_tensor: np.ndarray,
+    trotter_step: AsymmetricLowRankTrotterStepJW,
     time: float,
     n_steps: int,
-    final_rank: Optional[int],
-    spin_basis: bool,
 ) -> Iterator[tuple[Instruction, Sequence[Qubit]]]:
     step_time = time / n_steps
     for _ in range(n_steps):
-        yield AsymmetricLowRankTrotterStep(
-            one_body_tensor,
-            two_body_tensor,
-            step_time,
-            final_rank=final_rank,
-            spin_basis=spin_basis,
-        ), register
+        yield from trotter_step.trotter_step(register, step_time)
 
 
-class AsymmetricLowRankTrotterStep(QuantumCircuit):
+class AsymmetricLowRankTrotterStepJW:
     r"""Trotter Hamiltonian simulation via low rank decomposition.
 
     References:
@@ -119,87 +112,53 @@ class AsymmetricLowRankTrotterStep(QuantumCircuit):
         self,
         one_body_tensor: np.ndarray,
         two_body_tensor: np.ndarray,
-        time: float,
         final_rank: Optional[int] = None,
         spin_basis: bool = False,
-        *,
-        qubit_converter: Optional[QubitConverter] = None,
-        **circuit_kwargs,
     ) -> None:
         r"""
         Args:
             one_body_tensor: The one-body tensor of the Hamiltonian.
             two_body_tensor: The two-body tensor of the Hamiltonian.
             time: Simulation time.
-            qubit_converter: The qubit converter. The default behavior is to create
-                one using the call `QubitConverter(JordanWignerMapper())`.
-            circuit_kwargs: Keyword arguments to pass to the QuantumCircuit initializer.
         """
-        if qubit_converter is None:
-            qubit_converter = QubitConverter(JordanWignerMapper())
+        self.one_body_tensor, self.leaf_tensors, self.core_tensors = low_rank_decomposition(
+            one_body_tensor, two_body_tensor, final_rank=final_rank, spin_basis=spin_basis
+        )
 
-        n, _ = one_body_tensor.shape
-        register = QuantumRegister(n)
-        super().__init__(register, **circuit_kwargs)
+    def trotter_step(
+        self, register: QuantumRegister, time: float
+    ) -> Iterator[tuple[Instruction, Sequence[Qubit]]]:
+        n_qubits = len(register)
 
-        if isinstance(qubit_converter.mapper, JordanWignerMapper):
-            operations = _asymmetric_low_rank_trotter_step_jw(
-                register, one_body_tensor, two_body_tensor, time, final_rank, spin_basis
-            )
-            for gate, qubits in operations:
-                self.append(gate, qubits)
-        else:
-            raise NotImplementedError(
-                "Currently, only the Jordan-Wigner Transform is supported. "
-                "Please use "
-                "qiskit_nature.mappers.second_quantization.JordanWignerMapper "
-                "to construct the qubit mapper."
-            )
+        # compute basis change that diagonalizes one-body term
+        transformation_matrix, orbital_energies, _ = QuadraticHamiltonian(
+            self.one_body_tensor
+        ).diagonalizing_bogoliubov_transform()
 
+        # change to the basis in which the one-body term is diagonal
+        yield BogoliubovTransform(transformation_matrix.T.conj()), register
 
-def _asymmetric_low_rank_trotter_step_jw(
-    register: QuantumRegister,
-    one_body_tensor: np.ndarray,
-    two_body_tensor: np.ndarray,
-    time: float,
-    final_rank: Optional[int],
-    spin_basis: bool,
-) -> Iterator[tuple[Instruction, Sequence[Qubit]]]:
-    n_qubits = len(register)
-    # TODO do decomposition at higher level to avoid repeated work
-    corrected_one_body_tensor, leaf_tensors, core_tensors = low_rank_decomposition(
-        one_body_tensor, two_body_tensor, final_rank=final_rank, spin_basis=spin_basis
-    )
+        # simulate the one-body terms
+        for qubit, energy in zip(register, orbital_energies):
+            yield RZGate(-energy * time), (qubit,)
 
-    # compute basis change that diagonalizes one-body term
-    transformation_matrix, orbital_energies, _ = QuadraticHamiltonian(
-        corrected_one_body_tensor
-    ).diagonalizing_bogoliubov_transform()
+        # simulate the two-body terms
+        prior_transformation_matrix = transformation_matrix
+        for leaf_tensor, core_tensor in zip(self.leaf_tensors, self.core_tensors):
+            # change basis
+            merged_transformation_matrix = prior_transformation_matrix @ leaf_tensor.conj()
+            yield BogoliubovTransform(merged_transformation_matrix), register
+            # simulate off-diagonal two-body terms
+            for p, q in itertools.combinations(range(n_qubits), 2):
+                yield from _rot11((register[p], register[q]), -core_tensor[p, q] * time)
+            # simulate diagonal two-body terms
+            for p in range(n_qubits):
+                yield RZGate(-0.5 * core_tensor[p, p] * time), (register[p],)
+            # update prior basis change matrix
+            prior_transformation_matrix = leaf_tensor.T
 
-    # change to the basis in which the one-body term is diagonal
-    yield BogoliubovTransform(transformation_matrix.T.conj()), register
-
-    # simulate the one-body terms
-    for qubit, energy in zip(register, orbital_energies):
-        yield RZGate(-energy * time), (qubit,)
-
-    # simulate the two-body terms
-    prior_transformation_matrix = transformation_matrix
-    for leaf_tensor, core_tensor in zip(leaf_tensors, core_tensors):
-        # change basis
-        merged_transformation_matrix = prior_transformation_matrix @ leaf_tensor.conj()
-        yield BogoliubovTransform(merged_transformation_matrix), register
-        # simulate off-diagonal two-body terms
-        for p, q in itertools.combinations(range(n_qubits), 2):
-            yield from _rot11((register[p], register[q]), -core_tensor[p, q] * time)
-        # simulate diagonal two-body terms
-        for p in range(n_qubits):
-            yield RZGate(-0.5 * core_tensor[p, p] * time), (register[p],)
-        # update prior basis change matrix
-        prior_transformation_matrix = leaf_tensor.T
-
-    # undo final basis change
-    yield BogoliubovTransform(prior_transformation_matrix), register
+        # undo final basis change
+        yield BogoliubovTransform(prior_transformation_matrix), register
 
 
 def _rot11(qubits: Sequence[Qubit], angle: float) -> Iterator[tuple[Instruction, Sequence[Qubit]]]:
